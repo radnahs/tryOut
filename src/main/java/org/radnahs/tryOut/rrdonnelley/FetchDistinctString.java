f3a6e8e38d16f00612398b6f0672ec00dbe9fe0c
Shantanu Sikdar <sikdar.shantanu@gmail.com>
2025-03-20 19:38:41 +0530

    added stream api, though I added MAP impelementation but YAGNI principle doesnt suggest. as MAP way is over thinking.

--- src/main/java/org/radnahs/tryOut/rrdonnelley/FetchDistinctString.java
+++ src/main/java/org/radnahs/tryOut/rrdonnelley/FetchDistinctString.java
@@ -20,6 +20,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 
@@ -29,13 +30,19 @@ public class FetchDistinctString {
 	public static void main(String[] args) {
 
 		String arr[] = { "abcd", "java", "dcba", "ajav", "xyz", "epam", "pame", "aepm" };
+		Arrays.stream(arr).forEach(i -> {
+			System.out.println(i);
+		});
+		System.out.println("");
 		usingSET(arr);
+		usingStreamSET(arr);
 		System.out.println("");
 		usingMAP(arr);
+		usingStreamMAP(arr);
 	}
 
 	private static void usingSET(String arr[]) {
-		Set<String> setStr = new HashSet();
+		Set<String> setStr = new HashSet<>();
 		for (int j = 0; j < arr.length; j++) {
 			char[] charArr = arr[j].toCharArray();
 			Arrays.sort(charArr);
@@ -43,24 +50,55 @@ public class FetchDistinctString {
 		}
 		System.out.println(setStr);
 	}
-	
+
+	private static void usingStreamSET(String arr[]) {
+		Set<String> setStrStrm = new HashSet<>();
+		Arrays.stream(arr).forEach(str -> {
+			char[] charArr = str.toCharArray();
+			Arrays.sort(charArr);
+			setStrStrm.add(new String(charArr));
+		});
+		System.out.println(setStrStrm);
+	}
+
 	private static void usingMAP(String arr[]) {
 		Map<String, List<String>> mapStr = new HashMap<>();
 		for (int j = 0; j < arr.length; j++) {
 			char[] charArr = arr[j].toCharArray();
 			Arrays.sort(charArr);
 			String ss = new String(charArr);
-			if(mapStr.containsKey(ss)) {
+			if (mapStr.containsKey(ss)) {
 				List<String> lst = mapStr.get(ss);
-				lst.add(j+"_"+arr[j]);
+				lst.add(j + "_" + arr[j]);
 				mapStr.put(ss, lst);
-			}else {
+			} else {
 				List<String> lst = new ArrayList<>();
-				lst.add(j+"_"+arr[j]);
+				lst.add(j + "_" + arr[j]);
 				mapStr.put(ss, lst);
 			}
 		}
 		System.out.println(mapStr);
 	}
-	
+
+	private static void usingStreamMAP(String arr[]) {
+		Map<String, List<String>> mapStrStrm = new HashMap<>();
+		AtomicInteger j = new AtomicInteger(0);
+		Arrays.stream(arr).forEach(str -> {
+			char[] charArr = str.toCharArray();
+			Arrays.sort(charArr);
+			String ss = new String(charArr);
+			if (mapStrStrm.containsKey(ss)) {
+				List<String> lst = mapStrStrm.get(ss);
+				lst.add(j + "_" + str);
+				mapStrStrm.put(ss, lst);
+			} else {
+				List<String> lst = new ArrayList<>();
+				lst.add(j + "_" + str);
+				mapStrStrm.put(ss, lst);
+			}
+			j.addAndGet(1);
+		});
+		System.out.println(mapStrStrm);
+	}
+
 }
